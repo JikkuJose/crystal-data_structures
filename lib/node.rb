cc6be9dcac50d75cbd6f4d18d3383e6f5e7df613
Jikku Jose <jikkujose@gmail.com>
2017-03-20 14:11:57 +0530

    Stablize chat fragment format

--- lib/node.rb
+++ lib/node.rb
@@ -2,12 +2,23 @@ require_relative './response.rb'
 
 module Crystal
   class Node
-    def initialize(text:, tags:, responses:)
+    def initialize(label:, text:, tags:, responses:)
       @text = text
       @tags = tags
       @responses = responses
+      @label = label
 
       @id = (1000000000*rand).round.to_s(16)[0..5]
+    end
+
+    def to_h
+      {
+        id: @id,
+        tags: @tags,
+        label: @label,
+        text: @text,
+        responses: @responses.map(&:to_h)
+      }
     end
   end
 end
